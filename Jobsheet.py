3d3bdf880cc9d3c32e3fc31782a828b52cac1176
Ahmad Zakaria Fathoni <60414357+azarafath@users.noreply.github.com>
2020-11-04 12:47:44 +0700

    Menambahkan Menu dan Pilihan 1
    Menambahkan menu dan pilihan 1 persegi berserta rumus

--- Jobsheet.py
+++ Jobsheet.py
@@ -4,3 +4,30 @@ from IPython.display import clear_output
 
 def cls() :
     clear_output(True)
+
+while True:
+    #MenuPilihan
+    print("Pilihan Program")
+    print("1. Luas Persegi")
+    print("2. Luas Persegi Panjang")
+    print("3. Luas Segitiga")
+    print("4. Luas Lingkaran")
+    print("5. Keliling Persegi")
+    print("6. Keliling Persegi Panjang")
+    print("7. Keliling Segitiga")
+    print("8. Keliling Lingkaran")
+    print("9. Exit")
+        
+    #input
+    pilih = int(input("Masukkan pilihan :"))
+    
+    cls()
+
+    if pilih == 1:
+        print("Menghitung Luas Persegi")
+        sisi = int(input("Masukkan Panjang Sisi :"))
+        luas = sisi * sisi
+        kel = 4 * sisi
+        print("Luas Persegi adalah", luas)
+     
+    else
