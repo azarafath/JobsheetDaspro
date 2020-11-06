186cceb2d7073f30d9bdbfa426a604c5fc586c48
Febri Ardana <73922828+FebriiArdn@users.noreply.github.com>
2020-11-06 07:33:57 +0700

    Menambah rumus keliling

--- Jobsheet.py
+++ Jobsheet.py
@@ -21,3 +21,19 @@ def luas_segitiga(a,t):
 def luas_lingkaran(r):
     luas = 3.14 * r * r
     return luas
+
+def keliling_persegi(s):
+    keliling = 4 * s
+    return keliling
+
+def keliling_persegi_panjang(p,l):
+    keliling = 2 * (p + l)
+    return keliling
+
+def keliling_segitiga(a,b,c):
+    keliling = a + b + c
+    return keliling
+
+def keliling_lingkaran(r):
+    keliling = 2 * 3.14 * r
+    return keliling
