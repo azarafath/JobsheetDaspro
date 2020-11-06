d9366b195b7a926f6cbe3f530733fffa3af78b6a
Ahmad Zakaria Fathoni <60414357+azarafath@users.noreply.github.com>
2020-11-06 07:41:26 +0700

    Menambah if keliling, exit, dan looping "ulang"

--- Jobsheet.py
+++ Jobsheet.py
@@ -59,7 +59,7 @@ while True :
     if pilih == 1:
         print("Menghitung Luas Persegi")
         s = int(input("Masukkan panjang sisi :"))
-        print("Luas Persegi :", luas_bujur_sangkar(s))
+        print("Luas Persegi :", luas_persegi(s))
         
     elif pilih == 2:
         print("Menghitung Luas Persegi Panjang")
@@ -77,4 +77,46 @@ while True :
         print("Menghitung Luas Lingkaran")
         r = int(input("Masukkan jari-jari :"))
         print("Luas Lingkaran :", luas_lingkaran(r))
+        
+    elif pilih == 5:
+        print("Menghitung Kelilins Persegi")
+        s = int(input("Masukkan sisi :"))
+        print("Kelilins Persegi :", keliling_persegi(s))
+        
+    elif pilih == 6:
+        print("Menghitung Keliling Persegi Panjang")
+        p = int(input("Masukkan panjang :"))
+        l = int(input("Masukkan lebar :"))
+        print("Keliling Persegi Panjang :", keliling_persegi_panjang(p,l))
+        
+    elif pilih == 7:
+        print("Menghitung Keliling Segitiga")
+        a = int(input("Masukkan panjang sisi A :"))
+        b = int(input("Masukkan panjang sisi B :"))
+        c = int(input("Masukkan panjang sisi C :"))
+        print("Keliling Segitiga :", keliling_segitiga(a,b,c))
+        
+    elif pilih == 8:
+        print("Menghitung Keliling Lingkaran")
+        r = int(input("Masukkan jari-jari"))
+        print("Keliling Lingkaran :", keliling_lingkaran(r))
+        
+    elif pilih == 9:
+        print("Terima kasih telah menggunakan tools ini")
+        print("Exit...")
+        break
+        
+    else:
+        print("Pilihan Anda Salah")
+        
+    ulang =""
+    
+    while ulang != "y" and ulang!= "t" :
+        cls()
+        ulang = input("Kembali ke menu utama [y/t] ? ")
+        
+    if ulang == "t":
+        cls()
+        print("Aplikasi Berhenti. Exit...")
+        break
         
