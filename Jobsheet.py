7f1c91c72c539cd497a6b838bd76e4a747644a9e
Ahmad Zakaria Fathoni <60414357+azarafath@users.noreply.github.com>
2020-11-06 07:31:15 +0700

    Import Fungsi clear dan Deklarasi Rumus Luas
    Deklarasi Rumus Luas Bidang

--- Jobsheet.py
+++ Jobsheet.py
@@ -1,2 +1,23 @@
 #PROGRAM MENGHITUNG LUAS DAN KELILING BANGUN DATAR
 #DIBUAT OLEH AHMAD ZAKARIA FATHONI & FEBRI ARDANA
+
+from IPython.display import clear_output
+
+def cls():
+    clear_output(True)
+    
+def luas_persegi(s):
+    luas = s * s
+    return luas
+
+def luas_persegi_panjang(p,l):
+    luas = p * l
+    return luas
+
+def luas_segitiga(a,t):
+    luas = a * t
+    return luas
+
+def luas_lingkaran(r):
+    luas = 3.14 * r * r
+    return luas
